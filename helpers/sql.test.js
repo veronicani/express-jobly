ad3049f6a5ed21b8bbe1983d63e2b53a6d3f2899
Crystal Tran <c.t.tran95@gmail.com>
2024-01-10 17:36:33 -0500

    created test for partial inputs for sqlForPartialUpdate

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -3,28 +3,42 @@
 const { sqlForPartialUpdate }= require("./sql");
 const { BadRequestError } = require("../expressError");
 
+// things to test for
+// works with partial that is valid
+// works with full data that is valid
+// does not work with empty data and error
+// does not pass for partial data that is invalid
+
 describe("sqlForPartialUpdate", function () {
-  test("works: with valid data", function () {
-    expect(res.locals).toEqual({
-      user: {
-        iat: expect.any(Number),
-        username: "test",
-        isAdmin: false,
+  test("works with partial data that is valid", function () {
+    const { setCols, values } = sqlForPartialUpdate(
+      {
+        "firstName": "Bob",
+        "lastName": "Laster"
       },
-    });
+      {
+        firstName: "first_name",
+        lastName: "last_name",
+      });
+
+    console.log("***setCols:", setCols);
+    console.log("***values:", values);
+
+    expect(setCols).toEqual('"first_name"=$1, "last_name"=$2');
+    expect(values).toEqual(["Bob", "Laster"]);
   });
 
-  test("works: no header", function () {
-    const req = {};
-    const res = { locals: {} };
-    authenticateJWT(req, res, next);
-    expect(res.locals).toEqual({});
-  });
+  // test("works: no header", function () {
+  //   const req = {};
+  //   const res = { locals: {} };
+  //   authenticateJWT(req, res, next);
+  //   expect(res.locals).toEqual({});
+  // });
 
-  test("works: invalid token", function () {
-    const req = { headers: { authorization: `Bearer ${badJwt}` } };
-    const res = { locals: {} };
-    authenticateJWT(req, res, next);
-    expect(res.locals).toEqual({});
-  });
+  // test("works: invalid token", function () {
+  //   const req = { headers: { authorization: `Bearer ${badJwt}` } };
+  //   const res = { locals: {} };
+  //   authenticateJWT(req, res, next);
+  //   expect(res.locals).toEqual({});
+  // });
 });
