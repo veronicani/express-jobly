09b667e094426928bccdd380deab8634ca2d3ca8
Crystal Tran <c.t.tran95@gmail.com>
2024-01-12 13:16:04 -0500

    imported ensureAdmin

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn } = require("../middleware/auth");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
